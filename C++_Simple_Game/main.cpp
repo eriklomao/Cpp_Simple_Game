e2dead2fc04a9b48eb25e945bf5521da0a2a0328
Iñigo Erik Loma-Osorio de Andres <ieriklomaosorio@gmail.com>
2023-08-27 14:54:17 +0200

    Creating the Window

--- C++_Simple_Game/main.cpp
+++ C++_Simple_Game/main.cpp
@@ -1,7 +1,49 @@
-#include <stdio.h>
 #include <iostream>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
 int main() {
 
-	std::cout << "Hello1" << std::endl;
+	//Window creation
+	sf::RenderWindow window(sf::VideoMode(640, 480), "My first game", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event ev;
+
+	//Game Loop
+	while (window.isOpen()) 
+	{
+		//Event polling
+		while (window.pollEvent(ev)) 
+		{
+			switch (ev.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+				case sf::Event::KeyPressed:
+					if (ev.key.code == sf::Keyboard::Escape)
+						window.close();
+					break;
+				default:
+					break;
+			}
+
+		}
+
+		//Update
+
+
+
+		//Render
+		window.clear(sf::Color::Blue); //clear old frame
+
+		//Draw your game
+		window.display(); //Tell app that window is done drawing
+
+	}
+
+	return 0;
 }
